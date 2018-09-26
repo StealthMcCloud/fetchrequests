d1f2b32d6ae87ecd6e6808c4df470f8c3ff85bb9
Clinton Johnson <dragoon1246224@gmail.com>
2018-09-26 14:59:25 -0400

    Done with and just need to make it look nice

--- script.js
+++ script.js
@@ -9,17 +9,12 @@ fetch("http://jservice.io/api/category?id=680")
         document.write("Category: " + titleElement, "Question: " + cluesElement.question)
     })
 
-// async function questionTwo(id) {
-//     const res = await fetch("http://jservice.io/api/category?id=309" + id)
-//     const data = await res.json()
+    async function questionTwo () {
+        const res = await fetch("http://jservice.io/api/category?id=309")
+        const data = await res.json()
 
-//     const postElementTwo = document.getElementById("questionTwo" + id)
-//     postElementTwo.dataset.title = postElementTwo.title
-//     postElementTwo.dataset.clue = postElementTwo.clue
-//     postElementTwo.querySelector("p").textContent = postElementTwo.title
-
-//     const postBodyTwo = document.createTextNode(postElementTwo.body)
-//     postElementTwo.appendChild(postBodyTwo)
-//   }
-
-//   questionTwo(0)
+        let titleElement = data.title
+        let cluesElement = data.clues[Math.floor(Math.random() * data.clues.length)]
+        document.write("Category: " + titleElement, "Question: " + cluesElement.question)
+    }
+    questionTwo()
